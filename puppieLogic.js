76583243fdf4f299a7bd0fc1a8a14f86a506805a
koala-skif <koalask@skiff.com>
2024-01-29 09:00:06 +0000

    c

--- puppieLogic.js
+++ puppieLogic.js
@@ -11,12 +11,12 @@ async function puppieLogic(res) {
       '--single-process',
       '--no-zigote',
     ],
-    executablePath:
-      process.env.NODE_ENV === 'production'
-        ? process.env.PUPPETERR_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-    headless: false,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    //headless: false,
   });
+  /*       process.env.NODE_ENV === 'production'
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(), */
   try {
     const [page] = await browser.pages();
 
